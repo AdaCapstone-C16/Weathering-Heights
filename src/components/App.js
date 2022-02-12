c830e8c9e3f23c19e31fb66e742c620d29eaf06c
Lia Gaetano <lgaetano19@gmail.com>
2022-02-12 06:43:55 -0800

    Updates App.js anad package.json

--- src/components/App.js
+++ src/components/App.js
@@ -88,7 +88,7 @@ function App() {
       //     signalDBPull={signalDBPull} />
 
         <div>
-          <Router>
+          <Router basename="/Weathering-Heights">
             
             <AuthProvider>
               <Navigation />
