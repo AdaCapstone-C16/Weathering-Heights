6958b5640e18874c06cb3ee6181a811a36554fe1
Lia Gaetano <lgaetano19@gmail.com>
2022-02-12 07:20:01 -0800

    lg/ debugs deployment routing tests <Link> in index.js

--- src/index.js
+++ src/index.js
@@ -2,12 +2,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Link } from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter basename="/Weathering-Heights">
+      <Link to="/login" />
       <App />
     </BrowserRouter>
   </React.StrictMode>,
