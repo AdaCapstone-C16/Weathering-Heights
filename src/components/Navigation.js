e22952f35f39e3b2475637581ad98ee5ed8eed28
Lia Gaetano <lgaetano19@gmail.com>
2022-02-12 18:08:23 -0800

    lg/ Navigation only shows Home on Login, Signup, and ForgotPassword endpoints

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,10 +1,21 @@
+import { useState } from 'react';
 import { Navbar, Nav, NavItem } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import { useAuth } from "../contexts/AuthContext";
 import '../components/stylesheets/Navigation.css';
 
 const Navigation = () => {
     const { currentUser } = useAuth()
+    
+    let homeOnlyNavView;
+    const homeOnlyViews = ['/signup', '/forgot-password', '/login'];
+    const location = useLocation();
+    if (homeOnlyViews.includes(location.pathname)) {
+      homeOnlyNavView = true;
+    } else {
+      homeOnlyNavView = false;
+    }
+    
 
     return (
         <Navbar id="navigation">
@@ -14,21 +25,23 @@ const Navigation = () => {
                   <Nav.Link id="home-nav-link" as={Link} to="/" >Home</Nav.Link>
                 </NavItem>
                 
+                {/* MyProfile auth conditional */}
                 {currentUser && 
                   <NavItem className="nav-item" eventkey={2} href="/my-profile">
                     <Nav.Link id="profile-nav-link" as={Link} to="/my-profile" >Profile</Nav.Link>
                   </NavItem>
                 }
-                
-                {currentUser && 
-                  <NavItem className="nav-item" eventkey={3} href="/login">
-                    <Nav.Link id="logout-nav-link" as={Link} to="/login">Login</Nav.Link>
+
+                {/* Login vs Logout */}
+                {(currentUser && !homeOnlyNavView) &&
+                  <NavItem className="nav-item" eventkey={4} href="/">
+                    <Nav.Link id="login-nav-link" as={Link} to="/">Logout</Nav.Link>
                   </NavItem>
                 }
 
-                {!currentUser && 
-                  <NavItem className="nav-item" eventkey={4} href="/">
-                    <Nav.Link id="login-nav-link" as={Link} to="/">Logout</Nav.Link>
+                {(!currentUser && !homeOnlyNavView) && 
+                  <NavItem className="nav-item" eventkey={3} href="/login">
+                    <Nav.Link id="logout-nav-link" as={Link} to="/login">Login</Nav.Link>
                   </NavItem>
                 }
 
