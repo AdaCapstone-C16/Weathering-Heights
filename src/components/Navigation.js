0664d0b2da0b5ce2534484e63abfba3bb2a05548
Lia Gaetano <lgaetano19@gmail.com>
2022-02-12 17:20:28 -0800

    lg/ Navigation link syling, no header yet

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,27 +1,36 @@
 import { Navbar, Nav, NavItem } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import { useAuth } from "../contexts/AuthContext";
+import '../components/stylesheets/Navigation.css';
 
 const Navigation = () => {
-    // TODO: NOT WORKING
     const { currentUser } = useAuth()
 
     return (
-        <Navbar bg="light" variant="light">
-            <Nav className="me-auto">
+        <Navbar id="navigation">
+            <Nav id="navbar" className="justify-content-end">
 
-                <NavItem eventkey={1} href="/">
-                  <Nav.Link as={Link} to="/" >Home</Nav.Link>
+                <NavItem className="nav-item" eventkey={1} href="/">
+                  <Nav.Link id="home-nav-link" as={Link} to="/" >Home</Nav.Link>
                 </NavItem>
-                <NavItem eventkey={2} href="/my-profile">
-                  <Nav.Link as={Link} to="/my-profile" >Profile</Nav.Link>
-                </NavItem>
-                <NavItem eventkey={3} href="/login">
-                  {!currentUser && <Nav.Link as={Link} to="/login">Login</Nav.Link>}
-                </NavItem>
-                <NavItem eventkey={4} href="/logout">
-                  {currentUser && <Nav.Link as={Link} to="/login">Logout</Nav.Link>}
-                </NavItem>
+                
+                {currentUser && 
+                  <NavItem className="nav-item" eventkey={2} href="/my-profile">
+                    <Nav.Link id="profile-nav-link" as={Link} to="/my-profile" >Profile</Nav.Link>
+                  </NavItem>
+                }
+                
+                {currentUser && 
+                  <NavItem className="nav-item" eventkey={3} href="/login">
+                    <Nav.Link id="logout-nav-link" as={Link} to="/login">Login</Nav.Link>
+                  </NavItem>
+                }
+
+                {!currentUser && 
+                  <NavItem className="nav-item" eventkey={4} href="/">
+                    <Nav.Link id="login-nav-link" as={Link} to="/">Logout</Nav.Link>
+                  </NavItem>
+                }
 
             </Nav>
         </Navbar>
